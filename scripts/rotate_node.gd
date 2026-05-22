af2ff9c5e300753d0ee237ce4b23917f69757f64
Hakeron <richardyerna@gmail.com>
2026-05-22 15:37:22 +0200

    random rotation & color + detection movement

--- scripts/rotate_node.gd
+++ scripts/rotate_node.gd
@@ -5,4 +5,17 @@ extends Node
 func _push_in_node(received_node: Array[Node3D]):
 	var euler : Vector3 = Vector3(randf(), randf(), randf())
 	for n in received_node:
-		n.rotate(euler, randf()*360)
+		n.rotate(euler.normalized(), randf()*360)
+		var childrens = get_node_and_all_childrens_not_recusively(n)
+		for child in childrens:
+			if child is MeshInstance3D:
+				print(child)
+				var material: StandardMaterial3D = child.get_surface_override_material(0)
+				material.albedo_color = Color(randf(),randf(),randf())
+
+static func get_node_and_all_childrens_not_recusively(node:Node) -> Array[Node]:
+	var nodes:Array[Node] = []
+	nodes.append(node)
+	for child in node.get_children():
+		nodes.append(child)
+	return nodes
